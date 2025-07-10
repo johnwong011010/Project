6d21fd04bf104c94b577b6faa6999971fded8708
john.vong <john.vong@DEV-VDI10>
2025-07-10 16:17:11 +0800

    處理了在模糊搜索時大小寫的問題

--- Project/Service/AreaService.cs
+++ Project/Service/AreaService.cs
@@ -17,7 +17,7 @@ namespace Project.Service
         public async Task<List<Area>> GetAllArea() => await _service.Find(_ => true).ToListAsync();
         public async Task<Area> GetAreaByID(string id) => await _service.Find(x => x._id == id).FirstOrDefaultAsync();
         public async Task<List<Area>> GetAreaByCompanyID(string cid) => await _service.Find(x => x.sub_company_id == cid).ToListAsync();//may have many area in this company
-        public async Task<Area> GetAreaByName(string name) => await _service.AsQueryable().Where(x => x.name.ToLower().Contains(name)).FirstOrDefaultAsync();
+        public async Task<Area> GetAreaByName(string name) => await _service.AsQueryable().Where(x => x.name.ToLower().Contains(name.ToLower())).FirstOrDefaultAsync();
         public async Task UpdateArea(string id, Area a) => await _service.ReplaceOneAsync(x => x._id == id, a);
         public async Task DeleteArea(string id, string isDelete)//markdown this area is delete already
         {
--- Project/Service/Sub_Company_Service.cs
+++ Project/Service/Sub_Company_Service.cs
@@ -15,7 +15,7 @@ namespace Project.Service
         }
         public async Task<List<sub_companies>> GetAllCompany() => await _service.Find(_ => true).ToListAsync();
         public async Task<sub_companies> GetCompany(string id) => await _service.Find(x => x._id == id).FirstOrDefaultAsync();
-        public async Task<sub_companies> GetCompanyByName(string name) => await _service.AsQueryable().Where(x => x.name.ToLower().Contains(name)).FirstOrDefaultAsync();
+        public async Task<sub_companies> GetCompanyByName(string name) => await _service.AsQueryable().Where(x => x.name.ToLower().Contains(name.ToLower())).FirstOrDefaultAsync();
         public async Task UpdateCompany(string id, sub_companies companies) => await _service.ReplaceOneAsync(x => x._id == id, companies);
         public async Task AddCompany(sub_companies companies) => await _service.InsertOneAsync(companies);
         public async Task DeleteCompany(string id) => await _service.DeleteOneAsync(x => x._id == id);
